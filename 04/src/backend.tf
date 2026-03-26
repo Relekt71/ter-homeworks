fc8f2299645ef9de2f1193aa58f43ecabe399485
Relekt71 <mixtape.cat@mail.ru>
2026-03-26 11:02:03 +0000

    feat: add remote state with S3 backend and lockfile

---
+++ 04/src/backend.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket         = "trofimovmc-neto-bucket"
+    key            = "terraform.tfstate"
+    region         = "ru-central1"
+    use_lockfile   = true
+    
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
+}
